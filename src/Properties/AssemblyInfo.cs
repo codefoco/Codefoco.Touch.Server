04d170182fbef5590331a740135df46ba07abba1
Vinicius Jarina <viniciusjarina@gmail.com>
2019-02-02 01:28:14 -0500

    Initial commit.

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("Touch.Server from Touch.Unit - https://github.com/spouliot/Touch.Unit")]
+
+
+[assembly: AssemblyDescription ("Touch Server")]
+[assembly: AssemblyCompany ("Codefoco")]
+[assembly: AssemblyProduct ("Touch.Server")]
+[assembly: AssemblyCopyright ("Copyright © Sebastien Pouliot 2018")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0+0.Branch.master.Sha.94b1daaa58cb285279aa1a01a6472e6526f6b266")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
